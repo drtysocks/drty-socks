a9ff2a67c3de9a19320ad5834bc2174bf0c618f9
adamjonas <adam.jonas@carrotcreative.com>
2013-12-15 18:46:26 -0500

    quant buttons

--- assets/js/main.coffee
+++ assets/js/main.coffee
@@ -1,5 +1,15 @@
-# require.config
-#   paths:
-#     jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min'
+require.config
+  paths:
+    jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min'
 
-# require ['jquery'], ($) ->
+require ['jquery'], ($) ->
+  $(".plus").on "click", ->
+    $('input').val parseFloat($('input').val()) + 1
+
+  $(".minus").on "click", ->
+    unless parseFloat($('input').val()) == 1
+      $('input').val parseFloat($('input').val()) - 1 unless $('input').val() == 1
+
+  $('button.stripe-button-el').on "click", ->
+    quant = parseFloat($('input').val())
+    $("script[data-name='Drty Socks']").attr('data-amount', quant * 1000).attr('data-name', "Drty Socks (#{quant * 10}.00)" ).attr('data-description', "Drty Socks" )
--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,26 @@
 (function() {
+  require.config({
+    paths: {
+      jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min'
+    }
+  });
 
+  require(['jquery'], function($) {
+    $(".plus").on("click", function() {
+      return $('input').val(parseFloat($('input').val()) + 1);
+    });
+    $(".minus").on("click", function() {
+      if (parseFloat($('input').val()) !== 1) {
+        if ($('input').val() !== 1) {
+          return $('input').val(parseFloat($('input').val()) - 1);
+        }
+      }
+    });
+    return $('button.stripe-button-el').on("click", function() {
+      var quant;
+      quant = parseFloat($('input').val());
+      return $("script[data-name='Drty Socks']").attr('data-amount', quant * 1000).attr('data-name', "Drty Socks (" + (quant * 10) + ".00)").attr('data-description', "Drty Socks");
+    });
+  });
 
 }).call(this);
